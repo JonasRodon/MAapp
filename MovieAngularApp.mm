e67b1b90a6dc88519222312a62b716a24d0ef9bb
Jonas Rodon <jonasrodon@gmail.com>
2017-02-05 05:34:16 +0100

    initial structure of the movie app

---
+++ MovieAngularApp.mm
@@ -0,0 +1,83 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="MovieAngularApp" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1486268182697"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
+<node TEXT="Generar html" POSITION="right" ID="ID_1815628245" CREATED="1486268185202" MODIFIED="1486268199133">
+<edge COLOR="#ff0000"/>
+<node TEXT="a&#xf1;adir Angular.js" ID="ID_984195796" CREATED="1486268201492" MODIFIED="1486268223173"/>
+<node TEXT="a&#xf1;adir bootstrap" ID="ID_1313969728" CREATED="1486268224313" MODIFIED="1486268276612"/>
+<node TEXT="ngApp" ID="ID_824119811" CREATED="1486268283619" MODIFIED="1486268293397"/>
+<node TEXT="Generar estructura de directorios" ID="ID_365164440" CREATED="1486268305908" MODIFIED="1486268321234">
+<node TEXT="templates" ID="ID_487083284" CREATED="1486268324824" MODIFIED="1486268332090">
+<node TEXT="popular" ID="ID_466392233" CREATED="1486268417695" MODIFIED="1486268422248"/>
+<node TEXT="upcoming" ID="ID_87913779" CREATED="1486268423498" MODIFIED="1486268430440"/>
+<node TEXT="nowPlaying" ID="ID_1126211351" CREATED="1486268434632" MODIFIED="1486268455371"/>
+<node TEXT="topRated" ID="ID_1193408610" CREATED="1486268456294" MODIFIED="1486268463745"/>
+</node>
+<node TEXT="img" ID="ID_1752656137" CREATED="1486268617516" MODIFIED="1486268620795">
+<node TEXT="logo.png" ID="ID_1635282440" CREATED="1486268625058" MODIFIED="1486268630916"/>
+</node>
+<node TEXT="sass" ID="ID_755148146" CREATED="1486268594035" MODIFIED="1486268599588">
+<node TEXT="styles.sass" ID="ID_1973116718" CREATED="1486268600895" MODIFIED="1486268612520"/>
+</node>
+<node TEXT="css" ID="ID_448603755" CREATED="1486268570968" MODIFIED="1486268576120">
+<node TEXT="styles.css" ID="ID_512090057" CREATED="1486268579013" MODIFIED="1486268586164"/>
+</node>
+<node TEXT="js" ID="ID_557327229" CREATED="1486268333298" MODIFIED="1486268335109">
+<node TEXT="App.js" ID="ID_1717478140" CREATED="1486268494722" MODIFIED="1486268504095"/>
+<node TEXT="Components.js" ID="ID_217428762" CREATED="1486268505052" MODIFIED="1486268556482"/>
+<node TEXT="Factories.js" ID="ID_314074135" CREATED="1486268517347" MODIFIED="1486268562915"/>
+</node>
+<node TEXT="index.html" ID="ID_1426150120" CREATED="1486268336548" MODIFIED="1486268342955"/>
+</node>
+</node>
+</node>
+</map>
